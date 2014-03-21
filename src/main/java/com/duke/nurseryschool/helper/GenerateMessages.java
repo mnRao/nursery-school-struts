d2dd642ece3e30eebbbc34b49b346c983907b740
seekingvengeance4solace <seekingvengeance4solace@7508f683-f206-364e-31f8-0146c70b930e>
2014-03-21 17:13:21 +0000

    - Automatically generate message keys from I18N property file

---
+++ src/main/java/com/duke/nurseryschool/helper/GenerateMessages.java
@@ -0,0 +1,73 @@
+package com.duke.nurseryschool.helper;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+import java.util.TreeSet;
+
+import com.duke.nurseryschool.PropertyLoader;
+
+public class GenerateMessages {
+	private static final String PACKAGE = "com.duke.nurseryschool.generated";
+	private static final String MESSAGE_FILE_PATH = "../java/com/duke/nurseryschool/generated/Messages.java";
+
+	public static void main(String[] args) {
+		System.out.println("Writing message class ...");
+		writeMessageClass();
+		System.out.println("Finished");
+	}
+
+	@SuppressWarnings("unchecked")
+	private static void writeMessageClass() {
+		List<String> sortedEnKeySet = sortKeys();
+		System.out.println(sortedEnKeySet.size() + " keys in total");
+
+		try {
+			File file = new File(MESSAGE_FILE_PATH);
+			BufferedWriter output = new BufferedWriter(new FileWriter(file));
+			output.write("package " + PACKAGE + ";");
+			output.newLine();
+			output.newLine();
+			output.write("public class Messages{");
+			output.newLine();
+			output.newLine();
+
+			for (String key : sortedEnKeySet) {
+				output.write("\tpublic static final String "
+						+ transformKey(key.toString()) + " = \""
+						+ key.toString() + "\";");
+				output.newLine();
+			}
+
+			output.newLine();
+			output.write("}");
+			output.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static List<String> sortKeys() {
+		PropertyLoader enPropertyLoader = new PropertyLoader(
+				"global.properties");
+		Properties globalENProperties = enPropertyLoader.getGlobalProperties();
+		Set<Object> enKeySet = globalENProperties.keySet();
+		// Sort
+		List<String> sortedEnKeySet = new ArrayList<String>(new TreeSet(
+				enKeySet));
+		return sortedEnKeySet;
+	}
+
+	private static String transformKey(String key) {
+		String transformedKey = key.trim().toUpperCase();
+		transformedKey = transformedKey.replace(".", "_");
+
+		return transformedKey;
+	}
+}
